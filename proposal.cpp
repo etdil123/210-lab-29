8476a59543120691edc1bc1d07592da06291a14f
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-06 01:20:32 -0800

    10 min commit

--- proposal.cpp
+++ proposal.cpp
@@ -1,6 +1,10 @@
 // Ethan Dilk | COMSC 210 | Lab 29 Proposal
-// include necessary headers
+// include necessary headers related to data structures
 
+// Definition for function to simulate a game for a team 
+    // parameters: map of team and list to update stats
+    // creates a variable to randomly assign how the team is going to play
+    // randomly create stats for points, rebounds, assists - add to the lists
 
 // Define main function
     // Initialize a map used to store team information - each team will have array of lists for points, rebounds, assists
@@ -14,3 +18,15 @@
             // put the stats into the proper list for the team
 
     // close file
+
+    // Time based simulation of the 25 game season
+        // For a loop of 25 times
+            // Go through each team on map 
+                // call the function to input stats for one game for the team
+                // print each teams performance in the game
+
+    // rank and display the teams at the end of the season to show season performance
+
+    // end the main function
+
+    
