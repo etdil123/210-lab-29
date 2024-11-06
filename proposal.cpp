f49ce0de850681f572566367a0b8fcc2007302de
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-06 01:12:51 -0800

    first commit

---
+++ proposal.cpp
@@ -0,0 +1,16 @@
+// Ethan Dilk | COMSC 210 | Lab 29 Proposal
+// include necessary headers
+
+
+// Define main function
+    // Initialize a map used to store team information - each team will have array of lists for points, rebounds, assists
+
+    // Open external file to read data on first 5 games of the season 
+        // print error and exit if file doesn't open
+    
+    // read data from external file and use to populate the map
+        // Store the initial team names into the map
+        // for each line - get the stats from the file
+            // put the stats into the proper list for the team
+
+    // close file
